7f9eac9829049bebc0aed593b1c67a2634ddb8b1
akpgaa <46481890+akpgaa@users.noreply.github.com>
2022-02-19 10:55:26 +0530

    bug

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,6 +1,6 @@
 const env = {
-    ACCESS_POINT: "http://localhost:5000",
-
+    // ACCESS_POINT: "http://localhost:5000",
+    ACCESS_POINT: "https://ksmart-express.herokuapp.com"
 };
 
 module.exports = env;
